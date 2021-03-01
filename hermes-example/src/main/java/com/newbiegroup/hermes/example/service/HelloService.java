c439c117b3c7f1a97c4d3d8493b31d550affe471
张勇 <yong.zhang@dmall.com>
2021-03-01 09:45:45 +0800

    Hermes 项目初始化

---
+++ hermes-example/src/main/java/com/newbiegroup/hermes/example/service/HelloService.java
@@ -0,0 +1,14 @@
+package com.newbiegroup.hermes.example.service;
+
+/**
+ * <p>Description: </p>
+ * <p>Company: http://www.dmall.com</p>
+ *
+ * @author yong.zhang@dmall.com
+ * @version 1.0.0
+ * @date 2020/10/11 17:05
+ */
+public interface HelloService {
+    String sayHello(String name);
+    String test();
+}
---
+++ hermes-example/src/test/java/com/newbiegroup/Test.java
@@ -0,0 +1,17 @@
+package com.newbiegroup;
+
+/**
+ * <p>Description: </p>
+ * <p>Company: https://www.yuque.com/newbiegroup</p>
+ *
+ * @author newbiegroup
+ * @version 1.0.0
+ * @date 2021/1/19 15:12
+ */
+public class Test {
+    public static void main(String[] args) {
+        String str="table_nameaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+        System.out.println(str.length());
+    }
+}
